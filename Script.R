37275006c8b6e6dd7a01e3045f16483d982d061e
rememate <fdomunozmartin@gmail.com>
2020-01-22 20:46:05 +0100

    Trabajo con el tema televisión

--- Script.R
+++ Script.R
@@ -1,1 +1,12 @@
-grep("televisión", noticias.df$articulo)
+# En los url se indica la pertenencia a la sección televisión, la sobrerrepresentación de LL en el corpus me ha hecho
+# coger tan solo 80 aleatorios de entre esos. 
+
+
+#TODO Me tengo que asegurar de que este es un buen método para extraer televisión como tema. Cine se puede estar mezclando
+# aunque también podría dejarlo así de momento porque de todas formas se diferencia bastante bien de crítica de arte.
+
+television <- Reduce(union, list(grep("television", noticias.df$url),
+                       sample(grep("cinetv", noticias.df$url), 80),
+                       grep("netflix", noticias.df$url),
+                       grep("peliculas", noticias.df$url)))
+
